1d8433624a281920172323418d336784269286db
Ali Neishabouri <ali.neishabouri@theactigraph.com>
2022-01-02 14:20:15 +0000

    Add vscode server

--- home.nix
+++ home.nix
@@ -16,5 +16,10 @@
     '';
   };
 
+  imports = [
+    "${fetchTarball {url="https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256="00aqwrr6bgvkz9bminval7waxjamb792c0bz894ap8ciqawkdgxp";}}/modules/vscode-server/home.nix"
+  ];
+
+  services.vscode-server.enable = true;
 }
 
