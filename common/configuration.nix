e5f806d3af4e343d26db897aaaa16eb4e159becb
Ali Neishabouri <ali@neishabouri.net>
2022-09-23 09:58:56 +0000

    Update hash

--- common/configuration.nix
+++ common/configuration.nix
@@ -8,7 +8,7 @@
   };
 
   imports = [
-    (fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "00aqwrr6bgvkz9bminval7waxjamb792c0bz894ap8ciqawkdgxp"; })
+    (fetchTarball { url = "https://github.com/msteen/nixos-vscode-server/tarball/master"; sha256 = "1qga1cmpavyw90xap5kfz8i6yz85b0blkkwvl00sbaxqcgib2rvv"; })
   ];
 
   services.vscode-server.enable = true;
